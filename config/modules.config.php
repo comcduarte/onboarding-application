a6b7305101e21dfe7e4cd5172689757a59da73b3
Christopher Duarte <christopher.duarte@middletownct.gov>
2023-10-20 10:06:14 -0400

    Add Sub Modules

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,7 +6,25 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\I18n',
+    'Laminas\Navigation',
+    'Laminas\Mvc\Plugin\FilePrg',
+    'Laminas\Mvc\Plugin\FlashMessenger',
+    'Laminas\Mvc\Plugin\Identity',
+    'Laminas\Mvc\Plugin\Prg',
+    'Laminas\Session',
+    'Laminas\Db',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
     'Laminas\Router',
     'Laminas\Validator',
+    'Acl',
+    'Annotation',
     'Application',
+    'Components',
+    'Report',
+    'Settings',
+    'User',
 ];
